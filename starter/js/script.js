b5d5409ad7d121635614154b3bf00b07e7dab64d
becket <rebeca.aruxandei@gmail.com>
2024-01-19 19:19:25 +0000

    Added starter code and .gitignore

---
+++ starter/js/script.js
@@ -0,0 +1,11 @@
+// 01. Display current day at the top of the calender when a user opens the planner.
+
+// 02. Present timeblocks for standard business hours when the user scrolls down.
+
+// 03. Color-code each timeblock based on past, present, and future when the timeblock is viewed.
+
+// 04. Allow a user to enter an event when they click a timeblock.
+
+// 05. Save event in local storage when the save button is clicked in that timeblock.
+
+// 06. Persist events between refreshes of a page.
