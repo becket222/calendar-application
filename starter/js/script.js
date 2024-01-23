eabee7bb07df1fcaac7bc4bdbace3b36d6fbf564
becket <rebeca.aruxandei@gmail.com>
2024-01-23 23:39:59 +0000

    Moved timeblocks to HTML & done colour coding function

--- starter/js/script.js
+++ starter/js/script.js
@@ -5,57 +5,42 @@ dayjs.extend(window.dayjs_plugin_advancedFormat)
 var currentDay = dayjs();
 $("#currentDay").text(currentDay.format("dddd, MMM Do YYYY"));
 
-// 02. Present timeblocks for standard business hours when the user scrolls down.
-var businessHours = [
-    {
-        timeblock: "8:00 AM",
-        task: "",
-    },
-    {
-        timeblock: "9:00 AM",
-        task: "",
-    },
-    {
-        timeblock: "10:00 AM",
-        task: "",
-    },
-    {
-        timeblock: "11:00 AM",
-        task: "",
-    },
-    {
-        timeblock: "12:00 PM",
-        task: "",
-    },
-    {
-        timeblock: "13:00 PM",
-        task: "",
-    },
-    {
-        timeblock: "14:00 PM",
-        task: "",
-    },
-    {
-        timeblock: "15:00 PM",
-        task: "",
-    },
-    {
-        timeblock: "16:00 PM",
-        task: "",
-    }
-];
+// 03. Color-code each timeblock based on past, present (red), and future (green) when the timeblock is viewed.
+function colourCode() {
+    // Assign current hour to variable
+    var currentHour = dayjs();
+    
+    $(".time-block").each(function() {
+        var timeSlot = parseInt($(this).attr("id").split(".hour")[1]);
 
-function workTimeblocks() {
-    for (var i = 0; i < businessHours.length; i++) {
-        // make a <div> for each timeblock and append to container <div>
-        var timeblockRow = $("<div></div>");
-        $('#container').append(timeblockRow); 
-    }
+        // If timeSlot is less than currentHour remove previous classes and add .past
+        if (timeSlot < currentHour) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        }
+
+        // If timeSlot is same as currentHour remove previous classes and add .present
+        else if (timeSlot === currentHour) {
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+        }
+
+        // Else add .future
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    });
 }
-// 03. Color-code each timeblock based on past, present, and future when the timeblock is viewed.
 
 // 04. Allow a user to enter an event when they click a timeblock.
 
 // 05. Save event in local storage when the save button is clicked in that timeblock.
 
 // 06. Persist events between refreshes of a page.
+
+
+colourCode();
